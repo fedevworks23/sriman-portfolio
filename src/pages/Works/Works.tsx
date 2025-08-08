0ef712401bbad32ecc3e0f2bb737f61c34724094
fedevworks23 <fedevworks@gmail.com>
2025-08-08 20:15:04 +0530

    projects card

--- src/pages/Works/Works.tsx
+++ src/pages/Works/Works.tsx
@@ -1,5 +1,16 @@
 function Works() {
-  return <div>Works</div>;
+  return (
+    <>
+      <div>
+        <div className="font-bold text-3xl">TV Shows</div>
+        <p>https://fedevworks23.github.io/react-tvshows-app/</p>
+      </div>
+      <div>
+        <div className="font-bold text-3xl">Products App</div>
+        <p>https://fedevworks23.github.io/react-products-store/</p>
+      </div>
+    </>
+  );
 }
 
 export default Works;
